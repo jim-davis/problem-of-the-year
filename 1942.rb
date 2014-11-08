2fa4042bcbbad2e57a3e0a28f4c0733b8b19b5a0
Jim Davis <jrd3@alum.mit.edu>
2014-11-08 13:03:59 -0500

    use lists

--- 1942.rb
+++ 1942.rb
@@ -3,35 +3,49 @@ digits = %w{1 9 4 2}
 COMMUTATIVE_BINARY_OPERATORS = %w{+ *}
 ORDERED_BINARY_OPERATORS = %w{- /}
 
+class Array
+  def except(elt)
+    self - [elt]
+  end
+end
+
 def binary_operations(a, b)
+  l=[]
   if a.is_a?(String) && b.is_a?(String)
-    yield(a+b)
-    yield(b+a)
+    l << a+b
+    l << b+a
   end
   COMMUTATIVE_BINARY_OPERATORS.each do |op|
-    yield([a, op, b])
+    l << [a, op, b]
   end
-  return
+  return l
   ORDERED_BINARY_OPERATORS.each do |op|
     yield("#{a} #{op} #{b}")
   end
   ORDERED_BINARY_OPERATORS.each do |op|
     yield("#{b} #{op} #{a}")
   end
+
 end
 
 def generate_expressions(operands)
+  l = []
   operands.each do |op|
-    others = operands - [op]
-    generate_expressions2(op, others){|x| yield(x)}
+    others = operands.except(op)
+    b = generate_expressions2(op, others)
+    l += b
   end
+  l
 end
 
-def generate_expressions2(a, others)
-  binary_operations(a, others.first){|x| yield(x)}
+def generate_expressions2(op, others)
+  l = []
+  others.each do |op2|
+    l +=  binary_operations(op, op2)
+    others.except(op2)
+  end
+  l
 end
   
-#binary_operations(digits.first, digits[1]){|s| puts s}
-
-generate_expressions(digits){|s| puts s.respond_to?(:join) ? s.join(" ") : s}
+generate_expressions(digits).each{|expr| puts expr.respond_to?(:join) ? expr.join(" ") : expr}
 

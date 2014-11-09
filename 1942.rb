4895703071ea5119e0050e4751f0718dcdb0be62
Jim Davis <jrd3@alum.mit.edu>
2014-11-08 23:58:09 -0500

    operand combination done

--- 1942.rb
+++ 1942.rb
@@ -1,52 +1,55 @@
 #!/c/Ruby193/bin/ruby
-digits = %w{1 9 4 2}
 
-COMMUTATIVE_BINARY_OPERATORS = %w{+ *}
-ORDERED_BINARY_OPERATORS = %w{- /}
+# todo remove duplicates
+# sort by (normalized-form) order 
+# normalized: use commutative to put lessor arg first.
+# evaluate and store by result
+# remove those that are not whole numbers > 0 <=100
+# print with braces but only when needed
 
-class Array
-  def except(elt)
-    self - [elt]
-  end
-end
+require "util"
+require "expressions"
+require "operators"
+require "pry"
 
-
-def binary_combinations(a, b)
-  list=[]
-  if a.is_a?(String) && b.is_a?(String)
-    list << a+b
-    list << b+a
-  end
-  COMMUTATIVE_BINARY_OPERATORS.each do |op|
-    list << [a, op, b]
-  end
-  ORDERED_BINARY_OPERATORS.each do |op|
-    list << [a, op, b]
-  end
-  ORDERED_BINARY_OPERATORS.each do |op|
-    list << [a, op, b]
-  end
-  list
-end
-
-def generate_expressions(operands)
+# Given a list of operands, e.g. ["1", "2", "3"]
+# return all the ways these can be combined lexically in order
+# e.g. [["1", "2", "3"], ["1", "23"], ["12", "3"], ["123"]]
+def operand_combinations(operands)
   if operands.length == 1
-    operands
+    [operands]
   else
     l = []
-    operands.each do |lhs|
-      generate_expressions(operands.except(lhs)).each do |rhs|
-        l+= binary_combinations(lhs, rhs)
+    lhs = operands[0]
+    operand_combinations(operands.except(lhs)).each do |rhs|
+      if !rhs.is_a?(Array)
+        binding.pry
       end
-    end
+      if rhs.is_a?(Array)
+        l << [lhs] + rhs
+      end
+      l << [lhs + rhs[0]] + rhs[1..rhs.length]
+      end
     l
   end
 end
 
-def stringify(expr)
-  expr.respond_to?(:join) ? expr.join(" ") : expr
+def build_expressions(operands)
+  list = []
+  if operands.length == 1
+    [Digit.new(operands[0])]
+  else
+    COMMUTATIVE_BINARY_OPERATORS.each do |op|
+    end
+  end
+  list
 end
 
-# FIXME: sort and remove duplicates
+if __FILE__ == $0
+  digits = %w{1 2 3 4}
+  #permutations(%w(1 2 3 4)).each{|p| puts puts "#{p.inspect}"}
+  operand_combinations(digits).each{|e| puts "#{e.inspect}"}
+  exprs = build_expressions(operand_combinations(digits))
+  #exprs.each do |e| puts "#{e} => #{e.evaluate}" end
 
-generate_expressions(digits).each{|e| puts stringify(e)}
+end
---
+++ expressions.rb
@@ -0,0 +1,74 @@
+class Expression
+  def evaluate
+  end
+end
+
+class Digit < Expression
+  attr_accessor :value
+  def evaluate
+    value.to_i
+  end
+  def initialize(v)
+    self.value=v
+  end
+  def to_s
+    value
+  end
+end
+
+class OpExpression < Expression
+  attr_accessor :operator, :operands
+  def evaluate
+    operator.evaluate(operands.map(&:evaluate))
+  end
+end
+
+class MonadicExpression < OpExpression
+  def initialize(operator, operand)
+    self.operator = operator
+    self.operands = [operand]
+  end
+end
+
+class BinaryExpression < OpExpression
+  def initialize(operator, operand1, operand2)
+    self.operator = operator
+    self.operands = [operand1, operand2]
+  end
+  def to_s
+    "(#{operands[0].to_s} #{operator.symbol} #{operands[1].to_s})"
+  end
+end
+
+class Operator
+  attr_accessor :symbol, :f
+  def initialize(symbol, f)
+    self.symbol = symbol
+    self.f = f
+  end
+end
+
+class BinaryOperator < Operator
+  def evaluate(operands)
+    f.call(operands[0], operands[1])
+  end
+  def is_commutative?
+    false
+  end
+end
+
+class CommutativeOperator < BinaryOperator
+  def is_commutative?
+    true
+  end
+end
+
+
+
+if __FILE__ == $0
+Digit.new("23").evaluate
+  #puts Plus.evaluate([1, 2])
+  expr = BinaryExpression.new(Plus, Digit.new("1"), 
+                              BinaryExpression.new(Times, Digit.new("2"), Digit.new("3")))
+  puts "#{expr.to_s} == #{expr.evaluate}"
+end

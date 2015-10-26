f81d30e6e3efb166aca11d7f265330cc00a297c2
Jim Davis <jrd3@alum.mit.edu>
2015-10-25 20:01:59 -0400

    add a unit test that found a bug

--- spec/util_spec.rb
+++ spec/util_spec.rb
@@ -5,10 +5,21 @@ describe Array do
   describe "except" do
     describe "if the element is not present" do
       it "returns array unmodified" do
-        arr = Array.new
+        arr = []
         arr.except(nil)
         expect(arr.length).to eq(0)
       end
+    end
+    describe "if the element is present" do
+      it "returns array without the element" do
+        arr = [0,1,2,4] 
+      end
+    end
+  end
+  describe "butfirst" do
+    it "returns all but the first element" do
+      arr = [0,1,2,3]
+      expect(arr.butfirst.length).to eq(3)
     end
   end
 end
@@ -17,6 +28,27 @@ describe "lexical_combinations" do
   describe "with empty list" do
     it "returns an empty list" do
       expect(lexical_combinations([])).to eql([])
+    end
+  end
+  describe "with list of 1" do
+    it "returns the same list" do
+      arr = ["x"]
+      expected = [["x"]]
+      expect(lexical_combinations(arr)).to eql(expected)
+    end
+  end
+  describe "with list of 2" do
+    it "returns two results" do
+      arr = ["a", "b"]
+      expected = [["a", "b"], ["ab"]]
+      expect(lexical_combinations(arr)).to eql(expected)
+    end
+  end
+  describe "with list of 3" do
+    it "returns as expected" do
+      arr = ["a", "b", "c"]
+      expected = [["a", "b", "c"], ["a", "bc"], ["b", "ac"], ["c", "ab"], ["abc"]]
+      expect(lexical_combinations(arr)).to eql(expected)
     end
   end
   

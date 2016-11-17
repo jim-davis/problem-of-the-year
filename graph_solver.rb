566b8a259743db005d58e7ed7fb7452ce07d93de
Jim Davis <jrd3@alum.mit.edu>
2016-11-16 22:38:53 -0500

    Just added comments

--- graph_solver.rb
+++ graph_solver.rb
@@ -10,11 +10,9 @@ require "operators"
 class GraphPotySolver < PotySolver
   attr_reader :arboretum
   def solve(digits, range)
-    puts "Solving for #{range}"
     super(digits, range)
 
     @arboretum = Arboretum.new(digits.map { |d| Digit.new(d.to_i)})
-
     gt = GraphTraverser.new(arboretum)
     gt.each do |operator, left, right| 
       consider(arboretum.add(BinaryExpression.new(operator, left, right)))
@@ -24,6 +22,8 @@ class GraphPotySolver < PotySolver
   end
 
   def consider(n)
+    # a node is a possible solution if it uses all four digits and has a value
+    # and if the value is interesting then it really IS a solution.
     if n.terminal? && n.alive?
       stats.countEvaluation
       v = n.value
@@ -35,14 +35,13 @@ class GraphPotySolver < PotySolver
 
 end
 
-# find node L such that 
+# Generate possible nodes (O, L, R)
 # L is not terminal
 # L is alive
 # there exists another node R
 # that is not terminal and is alive
-#  whose left ancestor is adjacent to the right ancestor of L
+# whose left ancestor is adjacent to the right ancestor of L
 # and there exists a binary operator O such there is no existing Node (O L R)
-
 class GraphTraverser
   attr_reader :arboretum
   include Enumerable
@@ -50,6 +49,7 @@ class GraphTraverser
     @arboretum = arboretum
   end
 
+  # invoke block repeatedly with candidate notes (O,L,R)
   def each (&block)
     progress = true
     while progress do
@@ -63,8 +63,10 @@ class GraphTraverser
             end
           end
         end
-        # ADD Monadic operators here.  Apply to leaves first, then
-        # to interior expressions
+        if ! progress
+          # ADD Monadic operators here.  Apply to leaves first, then
+          # to interior expressions
+        end
       end
     end
   end
